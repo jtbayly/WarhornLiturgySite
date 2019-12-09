cd9861f7926583eac6398b190b1554df1fab91ac
Joseph Bayly <jtbayly@gmail.com>
2019-12-09 12:00:00 -0500

    hoping for a miracle

--- _build.sh
+++ _build.sh
@@ -6,3 +6,5 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 
+sed -e "s/div.sourceCode/blockquote/" ./_book/libs/gitbook-2.6.7/js/plugin-clipboard.js > ./_book/libs/gitbook-2.6.7/js/plugin-clipboard_new.js
+mv ./_book/libs/gitbook-2.6.7/js/plugin-clipboard_new.js ./_book/libs/gitbook-2.6.7/js/plugin-clipboard.js
